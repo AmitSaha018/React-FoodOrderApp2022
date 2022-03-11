15839163753ddb0e34ab213f83d7f1d2c36cedc4
AmitSaha018 <31251262+AmitSaha018@users.noreply.github.com>
2022-03-11 11:49:46 +0530

    Inserting and styling Header cart button.

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CartIcon from '../Cart/CartIcon';
+import Classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = () => {
+  return (
+      <button className={Classes.button}>
+          <span className={Classes.icon}>
+              <CartIcon/>
+          </span>
+          <span>
+              Your Cart
+          </span>
+          <span className={Classes.badge}>
+              3
+          </span>
+    </button>
+  );
+}
+
+export default HeaderCartButton;
